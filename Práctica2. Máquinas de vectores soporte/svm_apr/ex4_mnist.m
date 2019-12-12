3d761c68b31810fefad9ee45b8adbe612ed40268
QUESADA ZARAGOZA <marqueza@EVIRL-030-OK.dsic.cloud>
2019-12-12 10:03:03 +0100

    fixed including octave workspace

--- Práctica2. Máquinas de vectores soporte/svm_apr/ex4_mnist.m
+++ Práctica2. Máquinas de vectores soporte/svm_apr/ex4_mnist.m
@@ -5,20 +5,19 @@ load ../../MNIST/train-labels-idx1-ubyte.mat.gz #xl
 load ../../MNIST/t10k-images-idx3-ubyte.mat.gz #Y 
 load ../../MNIST/t10k-labels-idx1-ubyte.mat.gz #yl
 
-k = 10;
-
+# k = 10;
 # apply pca
-[m,W]=pca(X);
-YR=(W(:,1:k)'*(Y-m)')';
-XR=(W(:,1:k)'*(X-m)')';
+# [m,W]=pca(X);
+# YR=(W(:,1:k)'*(Y-m)')';
+# XR=(W(:,1:k)'*(X-m)')';
+# WE END UP NOT USING PCA BECAUSE ITS NOT NECESSARY
 
 #train svm
-svm = svmtrain(xl, XR, '-t 1 -c 1000');
-predictions=svmpredict(telabels(i,:),tedata(i,:),clas(current_classifier),"-q");
-
+svm = svmtrain(xl, X, '-t 0 -c 1000');
+predictions=svmpredict(yl,Y,svm,"-q");
 #display results
 disp("Accuracy")
-accuracy = sum(predictions'==telabels)/length(telabels)
+accuracy = sum(predictions==yl)/length(yl)
 disp("Confidence interval")
-dag_interval=1.96*sqrt((accuracy*(1-accuracy))/length(telabels))
+interval=1.96*sqrt((accuracy*(1-accuracy))/length(yl))
 
